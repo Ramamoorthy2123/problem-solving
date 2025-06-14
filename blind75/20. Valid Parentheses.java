35ef7bf1ffcaf3557f2acb87bf8437eec157ddd5
RamDeveloper <96062083+Ramamoorthy2123@users.noreply.github.com>
2025-06-14 15:43:20 +0530

    Create 20. Valid Parentheses.java - Easy

---
+++ blind75/20. Valid Parentheses.java
@@ -0,0 +1,24 @@
+
+
+class Solution {
+    public boolean isValid(String s) {
+
+
+      Stack<Integer> stack = new Stack<>();
+      for(char ch : s.toCharArray(){
+        if( ch == '(' || ch == '{' || ch == '[')
+            stack.push(ch);
+        else{
+          if(stack.isEmpty()) return false;
+          char top = stack.pop();
+          if(
+            top != '(' && ch == ')' ||
+            top != '{' && ch == '}' ||
+            top != '[' && ch == ']' ||
+          )
+            return false;
+        }
+      }
+      return stack.isEmpty();
+    }
+}
